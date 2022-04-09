24af2e940e5f162f0996e2fc1589e5477d2a39ee
Caio Barbosa <ciruscaio@hotmail.com>
2022-04-09 12:43:50 -0300

    Pre e pos ++

--- Aula04/Aula04.cpp
+++ Aula04/Aula04.cpp
@@ -5,26 +5,56 @@ using namespace std;
 
 int main()
 {
+    //PRIMEIRA PARTE
+
     //Variaveis
-    string Nome;
-    int Idade;
-    bool TesteParada;
+    //string Nome;
+    //int Idade;
+    //bool TesteParada;
 
     //Impressao de tela
-    cout << "  /////////////////////////////\n";
-    cout << " ///    LOCADORA DO CAIO   ///\n";
-    cout << "/////////////////////////////\n";
-    cout << "Cadastro de Usuario: \n\n";
-    cout << "Nome do usuario: ";
-    cin >> Nome;
-    cout << "\n";
-    cout << "Idade: ";
-    cin >> Idade;
+    //cout << "  /////////////////////////////\n";
+    //cout << " ///    LOCADORA DO CAIO   ///\n";
+    //cout << "/////////////////////////////\n";
+    //cout << "Cadastro de Usuario: \n\n";
+    //cout << "Nome do usuario: ";
+    //cin >> Nome;
+    //cout << "\n";
+    //cout << "Idade: ";
+    //cin >> Idade;
 
-    cout << "\n" << "Confirmando a entrada: " << Nome << ", " << Idade;
+    //cout << "\n" << "Confirmando a entrada: " << Nome << ", " << Idade;
 
 
-    cout << "\n" << "Aperte qualquer tecla para sair.";
-    cin >> TesteParada;
+    //cout << "\n" << "Aperte qualquer tecla para sair.";
+    //cin >> TesteParada;
+
+
+
+
+    //SEGUNDAD PARTE
+
+    //int x = 5;
+    //int y = 10;
+    //bool r;
+    //x = x + 1;
+    //x += 1;
+
+    //r = (x >= y);
+
+    //cout << r;
+
+
+    //TERCEIRA PARTE
+    //string a = "caio";
+    //string b = " barbosa";
+   
+    //cout << ( a );
+
+
+    //QUARTA PARTE
+    int x = 1;
+    cout << "PRE" << (++x);
+    cout << "POS" << (x++);
 
 }
